521164a5693480892241e281d72fe7de08be3aa1
Dave Richardson <contactme@daverichardson.ca>
2023-06-19 19:55:15 -0500

    updating some horizontal scrolling, colours, styles, etc.

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -72,17 +72,17 @@ const Navigation = ({ }) => {
           }   w-full lg:inline-flex lg:flex-grow lg:w-auto`}
         >
           <div className="lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto">
-            <Link href="/">
+            <Link href="/" onClick={() => setActive(!active)}>
               <div className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-plum-300 dark:hover:bg-navy-300 hover:text-plum-900 dark:hover:text-navy-900">
                 Home
               </div>
             </Link>
-            <Link href="/rsvp">
+            <Link href="/rsvp" onClick={() => setActive(!active)}>
               <div className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-plum-300 dark:hover:bg-navy-300 hover:text-plum-900 dark:hover:text-navy-900">
                 RSVP
               </div>
             </Link>
-            <Link href="/contact">
+            <Link href="/contact" onClick={() => setActive(!active)}>
               <div className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-plum-300 dark:hover:bg-navy-300 hover:text-plum-900 dark:hover:text-navy-900">
                 Contact
               </div>
