ece21f94eb91fb5fd8e0ef3623c0662e9ad8599a
Dave Richardson <contactme@daverichardson.ca>
2023-07-05 20:52:06 -0500

    Adding sendgrid api

--- app/api/rsvp/route.js
+++ app/api/rsvp/route.js
@@ -1,4 +1,5 @@
 import { updateGuestAttendance } from 'utils/airtable';
+import { sendEmail } from 'utils/sendEmail';
 
 export async function POST(req, res) {
   try {
@@ -8,11 +9,27 @@ export async function POST(req, res) {
       return await updateGuestAttendance(reportingGuest.guest, reportingGuest.rsvp)
     })
 
+    console.log("rsvpData => ", rsvpData);
+
+    let emailBody = rsvpData.map((rG) => `${rG.guest}: ${rG.rsvp}`).join("\n");
+    console.log("emailBody => ", emailBody);
+
+    // notify me
+    let sgResponse = await sendEmail({
+      to: 'contactme@daverichardson.ca',
+      subject: 'RSVP from Guest',
+      text: `Guest RSVPs\n${emailBody}`,
+    });
+
+    console.log("sgResponse => ", sgResponse);
+
     return new Response(
       JSON.stringify({ response_records }),
       { status: 200 }
     )
   } catch (error) {
+    console.log("POST RSVP / Error => ", error);
+
     return new Response(
       JSON.stringify({ error }),
       { status: 500 }
